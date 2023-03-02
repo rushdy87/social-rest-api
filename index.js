51ff22b7791d4367093ba52598ef2d0920872f11
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-03-02 04:40:43 +0300

    Installation, Creating Express App, Connecting MongoDB, Middlewares, Creating User Model, Login and Register System

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+const PORT = 8800;
+
+const app = express();
+
+// Connections to MongoDB
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to database');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+
+app.listen(PORT, () => {
+  console.log('Backend Server is running on Port:', PORT);
+});
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: '',
+    },
+    coverPicture: {
+      type: String,
+      default: '',
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followins: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model('User', UserSchema);
